2b2915216c5869efd549c295860fa293272a754d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-31 12:38:06 -0400

    Set up and test Alan AI from alan.app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { useEffect } from "react";
+import alanBtn from '@alan-ai/alan-sdk-web';
+
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: "ea0ad6213a1e3077c0ee9e3212f2793d2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command }) => {
+        if (command === 'testCommand') {
+          alert('This code is excuted');
+        }
+      }
+    });
+  }, []);
+
   return (
     <div>
-      Test
+      <h1>AI News Application</h1>
     </div>
   );
 }
