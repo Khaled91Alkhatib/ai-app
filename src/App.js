81705e39f00126d3eb37bbb6d2d36b0e837950e1
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-01 20:03:03 -0400

    Add command brought from alan app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   useEffect(() => {
     alanBtn({
       key: "ea0ad6213a1e3077c0ee9e3212f2793d2e956eca572e1d8b807a3e2338fdd0dc/stage",
-      onCommand: ({ command }) => {
-        if (command === 'testCommand') {
-          alert('This code is excuted');
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          console.log(articles)
         }
       }
     });
